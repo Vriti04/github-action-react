581af1c6b322b0822b2296122ec799f03f784f4c
vriti04 <vriti4862.be22@chitkara.edu.in>
2025-05-06 11:31:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>hello</h1>
+      <h2>heeeellloooo</h2>
+    </div>
+  )
+}
+
+export default App
